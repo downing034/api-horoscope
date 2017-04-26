bf9af5034263a7996003b5df5044eccd0b8e829e
Paul Downing <downing034@gmail.com>
2017-04-25 17:29:18 -0700

    add post to twitter

--- horoscope.rb
+++ horoscope.rb
@@ -5,6 +5,16 @@ require 'oauth'
 require 'sinatra'
 
 get '/' do
-  @prediction = ['🍰', '💔', '💪', '🏆', '🐷', '🍕', '🤖'].sample
+  @prediction = HTTParty.get('https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&format=json')['query']['random'][0]['title']
   erb :index
+end
+
+post '/twitter' do
+  consumer = OAuth::Consumer.new(
+    ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET'],
+    { site: 'https://api.twitter.com', scheme: 'header' }
+  )
+  token_hash = { oauth_token: ENV['ACCESS_TOKEN'], oauth_token_secret: ENV['ACCESS_TOKEN_SECRET'] }
+  access_token = OAuth::AccessToken.from_hash(consumer, token_hash )
+  access_token.request(:post, 'https://api.twitter.com/1.1/statuses/update.json', status: params[:button])
 end
